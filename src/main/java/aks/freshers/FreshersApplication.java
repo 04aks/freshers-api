41f0de47c4089f32cc9a24c7988327121d8966d2
04aks <abdellahssehib@gmail.com>
2025-08-11 14:51:05 +0100

    Initial Commit

---
+++ src/main/java/aks/freshers/FreshersApplication.java
@@ -0,0 +1,13 @@
+package aks.freshers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aks/freshers/FreshersApplicationTests.java
@@ -0,0 +1,13 @@
+package aks.freshers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
